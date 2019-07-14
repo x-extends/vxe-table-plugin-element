243aa02fb96f60a92bbcf5e725fb78e0d23e064c
xuliangzhan <xu_liangzhan@163.com>
2019-07-14 20:43:41 +0800

    update

--- index.js
+++ index.js
@@ -116,8 +116,6 @@ function cellText (h, cellValue) {
 
 /**
  * 渲染函数
- * renderEdit(h, editRender, params, context)
- * renderCell(h, editRender, params, context)
  */
 const renderMap = {
   ElAutocomplete: {
