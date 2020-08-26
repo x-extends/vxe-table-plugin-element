65c1fc6144bc4416fcb3b20169e471721d88db12
xuliangzhan <xu_liangzhan@163.com>
2020-08-26 23:08:39 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -59,7 +59,7 @@ gulp.task('build_umd', function () {
       target: 'es6',
       lib: ['dom', 'es6']
     }))
-    .pipe(replace(`import XEUtils from 'xe-utils/methods/xe-utils';`, `import XEUtils from 'xe-utils';`))
+    .pipe(replace(`import XEUtils from 'xe-utils/ctor';`, `import XEUtils from 'xe-utils';`))
     .pipe(babel({
       moduleId: pack.name,
       presets: ['@babel/env'],
