ce6fe3aeed40581a4976f9e669c9706a8a0e8802
xuliangzhan <xu_liangzhan@163.com>
2023-09-17 15:48:40 +0800

    fix

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,7 @@
 import { VXETableCore } from 'vxe-table'
 
 /**
- * 基于 vxe-table 的表格适配插件，用于兼容 element-ui 组件库
+ * 基于 vxe-table 的表格适配插件，用于兼容 element-plus 组件库
  */
 export declare const VXETablePluginElement: {
   install (vxetable: VXETableCore): void
